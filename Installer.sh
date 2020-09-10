27671b1e7f4152bd29a682b9273da43aa81a4d04
RemApple <32689703+NganDii@users.noreply.github.com>
2020-09-10 22:14:24 +0530

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,20 @@
+echo "Make Twrp flasher for Qualcomm by Rem Apple"
+echo "Press enter to continue"
+read any
+rm -rf /sdcard/twrp_flasher
+mkdir /sdcard/twrp_flasher
+clear
+cd ~/
+git clone https://github.com/NganDii/twrp_flasher
+mv twrp_flasher/twrp_flasher.zip /sdcard/twrp_flasher/
+cd /sdcard/twrp_flasher
+unzip twrp_flasher.zip
+rm -rf twrp_flasher.zip
+echo "50% Completed"
+echo
+echo "Now copy your recovery.img file to sdcard/twrp_flasher"
+echo "And rename it to twrp.img"
+echo "PRESS ENTER WHEN DONE"
+read any
+zip -r rem_recovery.zip twrp_flasher/
+echo "Successful"
